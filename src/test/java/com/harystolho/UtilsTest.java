917f3fa3719a9d57f067342884368f47a386a3c1
Harystolho <haristolhogamer@gmail.com>
2018-09-13 16:20:08 -0300

    Removed tests that are useless

--- src/test/java/com/harystolho/UtilsTest.java
+++ src/test/java/com/harystolho/UtilsTest.java
@@ -21,7 +21,7 @@ public class UtilsTest {
 
 	@Test
 	public void checkVersion() {
-		assertEquals(PEUtils.VERSION, "0.4");
+		assertEquals(PEUtils.VERSION, "0.7");
 	}
 
 	@Test(expected = IllegalStateException.class)
